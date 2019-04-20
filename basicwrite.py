5318e5095499b8a199508c0b8254974b6b5fb9ef
nelson <nlsnchin@gmail.com>
2019-04-20 15:57:45 -0400

    Mid work in progress Save

--- basicwrite.py
+++ basicwrite.py
@@ -1,6 +1,6 @@
 import os, sys # for file path
 import re  # mod strings regex
-import fnmatch # to remove files in copy directory
+import fnmatch as wildcardhandler # to remove files in copy directory
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -17,18 +17,24 @@ del path
 # Convert column data to pd dataframe, and dataframe to Csv file.
 # Load csv into mysql table.  Load descriptive file into mysql table.  Link with common Identifier.
 
+#Bug exist:  code is NOT being executed within skip path, as only 1 dupe occures at a time.
+#Howver, when still in non skip path there is a bug where it fails to check for dupe, thus there will always be one off
+
 def format_file():
 
     DataDirectory = '/TestData'  # ) sources the path to the csv file for format standardization
 
-    # Define working directory
+    # Define working directory (Read file directory)
     working_dir = ''.join([os.getcwd(), DataDirectory, '/'])
 
-    # Define file directory for copy placement, make sure to not dupe any files in here
+    # Define file directory for copy placement, make sure to not dupe any files in here (write directory)
         #Todo: add in wild card handler to skip directory so it handles sub directories and files
 
-    skip_copy_directory_csv = ''.join([working_dir, 'Copy_Formatted_CSV_Files','/'])
-    skip_copy_directory_txt = ''.join([working_dir, 'Copy_Formatted_TXT_Files', '/'])
+    skip_copy_directory = ''.join([working_dir, 'Copy_Formatted_Files','/'])
+    #skip_copy_directory = ''.join([working_dir, 'Copy_Formatted_CSV_Files','/'])
+    #skip_copy_directory_txt = ''.join([working_dir, 'Copy_Formatted_TXT_Files', '/'])
+
+
 
     # ) walks thru every file/dir/root in working directory
 
@@ -39,13 +45,15 @@ def format_file():
         for filename in files:  # looks thru every file
 
             # if not skipdirectory then do the below logic
-            if current_path != skip_copy_directory_csv or current_path != skip_copy_directory_txt:
+            if current_path != wildcardhandler.fnmatch(skip_copy_directory, '*'):  # try and prevent any logic from executing in directory and sub/files not 100% successful
+                print(current_path)
+
                     #Todo:  try instead of adding it to harcoded path, instead add fnmatch to if logic in order
                     # to skip directory and prevent duplicates
 
                 # check if copy file already exist/ delete if old version/ create new if none
-                exist_dircheck = os.path.isdir(skip_copy_directory_csv)
-                exist_filecheck = os.path.isfile(''.join([skip_copy_directory_csv, filename]))
+                exist_dircheck = os.path.isdir(skip_copy_directory)
+                exist_filecheck = os.path.isfile(''.join([skip_copy_directory, filename]))
 
 
                 # Make files in new directory, Remove and remake if already there
@@ -53,16 +61,20 @@ def format_file():
                     if exist_filecheck:
 
                         try:
-                            os.remove(''.join([skip_copy_directory, filename]))
+                            os.remove(wildcardhandler.fnmatch(exist_filecheck,'*')) #remove dupes, does not preventtheir creation tho (!!!! failed !!!)
                         except:
                             pass
+
+                        file = open(''.join([skip_copy_directory, filename, '_formatted.csv']), 'w+') # (this is the write file for csv conversion moved inside if logic to fix dup bug
+                    else:
+                        file = open(''.join([skip_copy_directory, filename, '_formatted.csv']),'w+')  # (this is the write file for csv conversion moved inside if logic to fix dup bug
+
                 else:
                     os.mkdir(skip_copy_directory)
+                    file = open(''.join([skip_copy_directory, filename, '_formatted.csv']), 'w+') # (this is the write file for csv conversion moved inside if logic to fix
 
-                #not stripping .csv extension from file name due to recursive loop copy file multiplying like crazy that I dont feel like debugging
 
-                file = open(''.join([skip_copy_directory, filename, '_formatted.csv']),'w+') # (this is the write file for csv conversion)
-
+                # dupe bug still present
 
                 #Todo Write Text file (create file) code goes here
 
