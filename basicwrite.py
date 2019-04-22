ae35df98ef6f4b4ba4b3876f6cf3b0aa15ede8c2
nelson <nlsnchin@gmail.com>
2019-04-22 11:31:30 -0400

    Mid Progress Save

--- basicwrite.py
+++ basicwrite.py
@@ -17,8 +17,6 @@ del path
 # Convert column data to pd dataframe, and dataframe to Csv file.
 # Load csv into mysql table.  Load descriptive file into mysql table.  Link with common Identifier.
 
-#Bug exist:  code is NOT being executed within skip path, as only 1 dupe occures at a time.
-#Howver, when still in non skip path there is a bug where it fails to check for dupe, thus there will always be one off
 
 def format_file():
 
@@ -27,14 +25,7 @@ def format_file():
     # Define working directory (Read file directory)
     working_dir = ''.join([os.getcwd(), DataDirectory, '/'])
 
-    # Define file directory for copy placement, make sure to not dupe any files in here (write directory)
-        #Todo: add in wild card handler to skip directory so it handles sub directories and files
-
     skip_copy_directory = ''.join([working_dir, 'Copy_Formatted_Files','/'])
-    #skip_copy_directory = ''.join([working_dir, 'Copy_Formatted_CSV_Files','/'])
-    #skip_copy_directory_txt = ''.join([working_dir, 'Copy_Formatted_TXT_Files', '/'])
-
-    loopcount = int(0)
 
     # ) walks thru every file/dir/root in working directory
 
@@ -44,61 +35,33 @@ def format_file():
 
         for filename in files:  # looks thru every file
 
-            print(filename)
-            print(files)
-            print(''.join([os.getcwd(),'/',filename]))
-            test = ''.join([os.getcwd(),'/',filename])
-            print (root)
-            print (dirs)
-            print(os.path.isfile(test))
-            print(os.path.isfile(''.join([working_dir,'/',filename])))
-
-
             if os.path.isfile(''.join([working_dir,'/',filename])):  # added to fix iteration on directory  #must BE ABSOLUTE PATH
-                print(filename)
 
                 # if not skipdirectory then do the below logic
-                if current_path != wildcardhandler.fnmatch(skip_copy_directory, '*'):  # try and prevent any logic from executing in directory and sub/files not 100% successful
-                    print(current_path)
-
-                    print(loopcount)
-                    loopcount += 1
-
-                    #Todo:  try instead of adding it to harcoded path, instead add fnmatch to if logic in order
-                    # to skip directory and prevent duplicates
+                if current_path != wildcardhandler.fnmatch(skip_copy_directory, '*'):  # try and prevent any logic from executing in directory and sub/files
 
                     # check if copy file already exist/ delete if old version/ create new if none
                     exist_dircheck = os.path.isdir(skip_copy_directory)
                     exist_filecheck = os.path.isfile(''.join([skip_copy_directory, filename]))
 
-
-                    # Make files in new directory, Remove and remake if already there(# not the solution)
+                    # Make files in new directory, Remove and remake files if already there
                     if exist_dircheck:
                         if exist_filecheck:
 
                             try:
-                                os.remove(wildcardhandler.fnmatch(exist_filecheck,'*')) #remove dupes, does not preventtheir creation tho (!!!! failed !!!)
+                                os.remove(wildcardhandler.fnmatch(exist_filecheck,'*')) #remove dupes if any
                             except:
                                 pass
-
-                            file = open(''.join([skip_copy_directory, filename, '_formatted.csv']), 'w+') # (this is the write file for csv conversion moved inside if logic to fix dup bug
-                        else:
-                            file = open(''.join([skip_copy_directory, filename, '_formatted.csv']),'w+')  # (this is the write file for csv conversion moved inside if logic to fix dup bug
 
                     else:
                         os.mkdir(skip_copy_directory)
-                        if exist_filecheck:
 
-                            try:
-                                os.remove(wildcardhandler.fnmatch(exist_filecheck,'*')) #remove dupes, does not preventtheir creation tho (!!!! failed !!!)
-                            except:
-                                pass
+                         
 
-                            file = open(''.join([skip_copy_directory, filename, '_formatted.csv']), 'w+') # (this is the write file for csv conversion moved inside if logic to fix dup bug
-                        else:
-                            file = open(''.join([skip_copy_directory, filename, '_formatted.csv']),'w+')  # (this is the write file for csv conversion moved inside if logic to fix dup bug
+                    file = open(''.join([skip_copy_directory, filename, '_formatted.csv']),'w+')  # (this is the write file for csv conversion moved inside if logic to fix dup bug
 
-                    # dupe bug still present
+
+
 
                     #Todo Write Text file (create file) code goes here
 
