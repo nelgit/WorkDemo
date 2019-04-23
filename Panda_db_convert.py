9400b63521dccd3297416c7f35fe82c768234cde
nelson <nlsnchin@gmail.com>
2019-04-23 04:11:49 -0400

    add pandas dataframe converter based on created files in Copy Formatted Files Dir

--- Panda_db_convert.py
+++ Panda_db_convert.py
@@ -9,15 +9,15 @@ if not path in sys.path:
 del path
 
 
-def convert_to_pd_database():
+def convert_to_pd_dataframe():
 
-    import_dir = '/TestData/Copy_Formatted_Files/Test.csv'  # simple demo, not gonan go thru every file, just this test file
+    import_dir = '/TestData/Copy_Formatted_Files/Test_formatted.csv'  # simple demo, not gonna go thru every file, just this test file
 
     # Define working directory
     working_dir = ''.join([os.getcwd(), import_dir])
 
     print(working_dir)
 
-    created_db = pd.read_csv(working_dir, index_col=None)
+    created_db = pd.read_csv(working_dir, index_col=None, header=None)
 
     return created_db
